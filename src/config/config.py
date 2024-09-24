65a49b73788dc160d2cef0f09c3f2049d8825de0
carjiso <cjimenez@indigitalsolutions.com>
2024-09-24 00:20:29 -0500

    update config

---
+++ src/config/config.py
@@ -0,0 +1,1 @@
+DATABASE_URL = "mysql+pymysql://user:password@localhost/user_info_db"
--- src/config/database.py
+++ src/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Pollodepelea1*@localhost/user_info_db"
+from src.config.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
